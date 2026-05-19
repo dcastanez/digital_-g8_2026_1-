3f456245630c3752771d96f68442bc69c7e553e5
dcastanez <dcastanez@unal.edu.co>
2026-05-19 12:11:59 -0500

    Add send_n_leds module for LED control

---
+++ codigos/send_n_leds.v
@@ -0,0 +1,22 @@
+module send_n_leds(clk, rst, init, done)
+  input rst;
+  input clk;
+  input init_m;
+  input rst_cmd;
+  output done_m;
+
+  wire w_rst_cmd;
+  wire w_init_led
+  wire w_rst;
+  wire w_inc;
+  
+  wire w_addr;
+  wire w_rgb;
+
+  wire w_done_led;
+  wire w_z;  
+
+  count_addr conut_adrr0 (.clk(clk), .load(w_rst), .inc(w_inc), .addr(w_addr));
+  comp comp0 (.clk(clk), .addr(w_addr), .z(w_z));
+  led_mem led_mem0 (.addr(w_addr), .rgb(w_rgb));
+  ws2812 ws28120 (.clk(clk), .init(w_init_led), .rgb(w_rgb), .rst_cmd(w_rst_cmd, .dout(w_done_led), .done_led(w_done_led)
