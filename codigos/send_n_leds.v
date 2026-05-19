49151b18b7d541bb159ebd7e49e206954ca74812
dcastanez <dcastanez@unal.edu.co>
2026-05-19 12:42:56 -0500

    Refactor send_n_leds module inputs and connections

--- codigos/send_n_leds.v
+++ codigos/send_n_leds.v
@@ -1,12 +1,11 @@
-module send_n_leds(clk, rst, init, done);
-  input rst;
+module send_n_leds(clk, init_m, rst_cmd, done_m, dout);
+
   input clk;
   input init_m;
   input rst_cmd;
   output done_m;
   output dout;
 
-  wire w_rst_cmd;
   wire w_init_led;
   wire w_rst;
   wire w_inc;
@@ -20,7 +19,7 @@ module send_n_leds(clk, rst, init, done);
   count_addr conut_adrr0 (.clk(clk), .load(w_rst), .inc(w_inc), .addr(w_addr));
   comp comp0 (.clk(clk), .addr(w_addr), .z(w_z));
   led_mem led_mem0 (.addr(w_addr), .rgb(w_rgb));
-  ws2812 ws28120 (.clk(clk), .init(w_init_led), .rgb(w_rgb), .rst_cmd(w_rst_cmd), .dout(w_done_led),
+  ws2812 ws28120 (.clk(clk), .init(w_init_led), .rgb(w_rgb), .rst_cmd(rst_cmd), .dout(w_done_led),
                   .done_m(w_done_led));
 
   endmodule
