8d363cc718a80eb31f9df220d126a8d90e5bf8f1
dcastanez <dcastanez@unal.edu.co>
2026-05-26 20:33:59 -0500

    Implement state machine for control_MULT
    Added state machine parameters and initial conditions.

--- MULTIPLICADOR/control_MULT.v
+++ MULTIPLICADOR/control_MULT.v
@@ -9,3 +9,48 @@ module control_MULT(
                     output ad,
                     output done
                     );
+
+parameter CHECK = 3'b000;
+parameter START = 3'b001;
+parameter SHIFT = 3'b010;
+parameter ADD   = 3'b011;
+parameter DONE  = 3'b100;
+
+reg [2:0]state;
+
+initial begin
+  rs = 0;
+  sh = 0;
+  ad = 0;
+  done = 0;
+
+  state = 0;
+end
+
+reg [3:0]count;
+
+  always @(posedge clk) begin
+    if (rs)
+      state = CHECK;
+    else
+      case(state)
+
+      CHECK:begin
+        count=0;
+        if (init)
+          state = START;
+        else
+          state = CHECK;
+      end
+
+      START:
+        if (B)
+          state = ADD;
+        else
+          state = SHIFT;
+
+      SHIFT:
+        if (B)
+          state = ADD;
+        else
+          state = SHIFT;
