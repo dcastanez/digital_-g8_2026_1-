cc2483e348fe5763626e501dfd270f9719786122
dcastanez <dcastanez@unal.edu.co>
2026-06-01 11:17:33 -0500

    Update DIV.v

--- DIVISOR/DIV.v
+++ DIVISOR/DIV.v
@@ -2,13 +2,19 @@ module DIV(
            input clk,
            input init,
            input rst,
-           input A,
-           input B,
-           output R,
+           input [15:0] A,
+           input [15:0] B,
            output done);
 
            wire w_rs;
            wire w_sh;
-           wire w_ad;
+           wire w_de;
+           wire [31:0] w_R;
+           wire w_j;
 
-  rsr
+           lsr lsr0 (.clk(clk), .rs(w_rs), .sh(w_sh), .i_A(A),
+                     .R(w_R));
+           comp comp0 (.B(B)
+                       .j(w_j));
+           dec dec0 (.clk(clk), .rs(w_rs), .ad(w_ad), .j(w_j),
+                     .R(w_R))
