973ee2485246738897b3edd1a6778fa8c2e99f72
dcastanez <dcastanez@unal.edu.co>
2026-06-01 09:13:55 -0500

    Create DIV module with inputs and outputs

---
+++ DIVISOR/DIV.v
@@ -0,0 +1,14 @@
+module DIV(
+           input clk,
+           input init,
+           input rst,
+           input A,
+           input B,
+           output R,
+           output done);
+
+           wire w_rs;
+           wire w_sh;
+           wire w_ad;
+
+  rsr
